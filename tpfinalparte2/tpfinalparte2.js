23130f68f75b7c80ec520b81ccfe9c9aa407769f
martinnico2007 <martinnicolas140507@gmail.com>
2025-11-15 14:42:06 -0300

    Update tpfinalparte2.js

--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -32,16 +32,14 @@ mijuego.ui.cargarFuentes(fuenteBotones);
 }
 
 function draw() {
-  // En lugar de tener el IF aquí, llamamos a un método de la clase.
   mijuego.actualizarYDibujar();
 }
 
 function keyPressed() {
-  // Delegamos el evento de tecla a la clase.
   mijuego.manejarTecla(keyCode);
 }
 
 function mousePressed() {
-  // Delegamos el evento de clic a la clase.
   mijuego.manejarClic(mouseX, mouseY);
 }
+
